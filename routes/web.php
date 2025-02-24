cf403cdee64fe328d3255129a837b92bfecfd4f0
PassPTR <118502435+PassPTR@users.noreply.github.com>
2025-02-24 22:38:09 +0700

    edit

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/home', [PagesController::class, 'index'])->name('index');
 Route::get('/create', [PagesController::class, 'create'])->name('create');
 Route::post('/create', [PagesController::class, 'inputdata'])->name('inputdata');
 Route::get('/edit/{id}', [PagesController::class, 'edit'])->name('edit');
-Route::put('/update/{id}', [PagesController::class, 'update'])->name('update');
-Route::delete('/delete/{id}', [PagesController::class, 'delete'])->name('delete');
+// Route::put('/update/{id}', [PagesController::class, 'update'])->name('update');
+// Route::delete('/delete/{id}', [PagesController::class, 'delete'])->name('delete');
 // Route::get('/edit', [PagesController::class, 'edit'])->name('edit');
